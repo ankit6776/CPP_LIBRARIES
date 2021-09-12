ced081dac1b5f5b39b6acdaf25e46f6955b895bd
ankit6776 <37286264+ankit6776@users.noreply.github.com>
2021-09-12 12:21:14 +0530

    point update

--- src/segTree.hpp
+++ src/segTree.hpp
@@ -4,7 +4,7 @@ template<typename T> inline T Max(T a, T b){return a>b?a:b;}
 template<typename T> inline T Min(T a, T b){return a<b?a:b;}
 template<typename T> inline T __Gcd(T a, T b){ return b==0 ? a : __Gcd(b,a%b);}
 template<typename T> struct SegTree {
-    long long N;
+    long long int N;
     vector<T> v;
     vector<T> seg;
     void init(int st, int end, int ind){
@@ -19,17 +19,31 @@ template<typename T> struct SegTree {
     }
     T f(T a, T b){
         //define function here, here fucntion is sum
-        return a+b;
+        return a+b;  // uncomment for sum query
         
-        //for gcd
-        // return __gcd(a,b);
-        
-        // for maxm
-        // return max(a,b);
     }
     void pre() {
         seg.resize(4*N);
         init(0,N-1,0);
+    }
+    
+    void point_update(int p, T x) {
+        point_update_query(0,0,N-1,p,x);
+    }
+    
+    T update_function(T x, T y){
+        return x+y;
+    }
+    
+    void point_update_query(int node, int st, int en, int ind, T x) {
+        if(ind<st||ind>en)return;
+        if(st==en) {
+            seg[node] = update_function(seg[node], x);
+            return;
+        }
+        int mid = (st+en)/2;
+        ind<=mid ? point_update_query(2*node+1, st, mid, ind, x) : point_update_query(2*node+2, mid+1, en, ind, x);
+        seg[node]=f(seg[2*node+1],seg[2*node+2]);
     }
     
     T query(int l,int r){
@@ -37,10 +51,11 @@ template<typename T> struct SegTree {
     }
     
     T _query(int node, int l,int r, int st,int en){
-        if(l>en||r<st)return 0;
+        if(l>en||r<st)return 0; // replace INF with zero for sum query
         if(st>=l&&en<=r)return seg[node];
         int mid = (st+en)/2;
         return f(_query(2*node+1,l,r,st,mid),_query(2*node+2,l,r,mid+1,en));
     }
     
 };
+
